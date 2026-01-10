bbcb61160135d8c2d1e494d6a14e10be47e14451
maxmttang <maxmttang@gmail.com>
2026-01-10 10:46:10 -0800

    Add bcst-schedule.js with training schedule data

---
+++ sw/bcst-schedule.js
@@ -0,0 +1,238 @@
+// ================================================================================
+// BC SWIM TEAM SCHEDULE DATA
+// ================================================================================
+
+window.BC_TRAINING_SCHEDULE = {
+    season: "Fall - Spring 2025-2026",
+    note: "(R) = Rec pool",
+    groups: [
+        {
+            name: "National",
+            schedule: {
+                Monday: ["5:00-7:00am", "3:30-5:30pm"],
+                Tuesday: ["5:00-7:00am", "3:30-5:30pm"],
+                Wednesday: ["5:00-7:00am", "3:30-5:30pm"],
+                Thursday: ["5:00-7:00am", "3:30-5:30pm"],
+                Friday: ["5:00-7:00am", "3:30-5:30pm"],
+                Saturday: ["7:00-9:30am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: ["3:00-3:30pm"],
+                Tuesday: [],
+                Wednesday: ["3:00-3:30pm"],
+                Thursday: [],
+                Friday: ["3:00-3:30pm"],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Senior Performance",
+            schedule: {
+                Monday: ["5:00-7:00am", "3:30-5:30pm"],
+                Tuesday: ["5:00-7:00am", "3:30-5:30pm"],
+                Wednesday: ["5:00-7:00am", "3:30-5:30pm"],
+                Thursday: ["5:00-7:00am", "3:30-5:30pm"],
+                Friday: ["5:00-7:00am", "3:30-5:30pm"],
+                Saturday: ["7:00-9:30am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: ["3:00-3:30pm"],
+                Tuesday: [],
+                Wednesday: ["3:00-3:30pm"],
+                Thursday: [],
+                Friday: ["3:00-3:30pm"],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Senior 1",
+            schedule: {
+                Monday: ["3:30-5:30pm"],
+                Tuesday: ["3:30-5:30pm"],
+                Wednesday: ["3:30-5:30pm"],
+                Thursday: ["3:30-5:30pm"],
+                Friday: ["3:30-5:30pm"],
+                Saturday: ["7:00-9:00am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: ["3:00-3:30pm"],
+                Tuesday: [],
+                Wednesday: ["3:00-3:30pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Senior 2",
+            schedule: {
+                Monday: ["3:30-5:30pm"],
+                Tuesday: ["3:30-5:30pm"],
+                Wednesday: ["3:30-5:30pm"],
+                Thursday: ["3:30-5:30pm"],
+                Friday: [],
+                Saturday: ["7:00-9:00am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: [],
+                Tuesday: [],
+                Wednesday: ["3:00-3:30pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Prep",
+            schedule: {
+                Monday: ["4:00-6:00pm"],
+                Tuesday: ["4:00-6:00pm"],
+                Wednesday: ["4:00-6:00pm"],
+                Thursday: ["4:00-6:00pm"],
+                Friday: ["4:00-6:00pm"],
+                Saturday: ["9:30-11:30am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: ["3:30-4:00pm"],
+                Tuesday: [],
+                Wednesday: ["3:30-4:00pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Regional",
+            schedule: {
+                Monday: ["4:00-5:45pm"],
+                Tuesday: ["4:00-5:45pm"],
+                Wednesday: ["4:00-5:45pm"],
+                Thursday: ["4:00-5:45pm"],
+                Friday: [],
+                Saturday: ["9:30-11:15am"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: [],
+                Tuesday: [],
+                Wednesday: ["3:30-4:00pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Champs",
+            schedule: {
+                Monday: ["4:00-5:30pm"],
+                Tuesday: ["4:00-5:30pm"],
+                Wednesday: ["4:00-5:30pm"],
+                Thursday: ["4:00-5:30pm"],
+                Friday: [],
+                Saturday: ["11:30am-1:00pm"],
+                Sunday: []
+            },
+            dryland: {
+                Monday: [],
+                Tuesday: [],
+                Wednesday: [],
+                Thursday: [],
+                Friday: [],
+                Saturday: [],
+                Sunday: []
+            }
+        },
+        {
+            name: "Divisional",
+            schedule: {
+                Monday: ["4:00-5:15pm"],
+                Tuesday: ["4:00-5:15pm"],
+                Wednesday: ["4:00-5:15pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["11:30am-12:45pm"],
+                Sunday: []
+            },
+            dryland: null
+        },
+        {
+            name: "Orange",
+            schedule: {
+                Monday: ["4:00-5:00pm"],
+                Tuesday: ["4:00-5:00pm"],
+                Wednesday: ["4:00-5:00pm"],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["1:00-2:00pm"],
+                Sunday: []
+            },
+            dryland: null
+        },
+        {
+            name: "Gold 1",
+            schedule: {
+                Monday: ["4:00-4:45pm (R)"],
+                Tuesday: ["4:00-4:45pm (R)"],
+                Wednesday: ["4:00-4:45pm (R)"],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["1:00-1:45pm (R)"],
+                Sunday: []
+            },
+            dryland: null
+        },
+        {
+            name: "Gold 2",
+            schedule: {
+                Monday: ["4:45-5:30pm (R)"],
+                Tuesday: ["4:45-5:30pm (R)"],
+                Wednesday: ["4:45-5:30pm (R)"],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["1:45-2:30pm (R)"],
+                Sunday: []
+            },
+            dryland: null
+        },
+        {
+            name: "Silver",
+            schedule: {
+                Monday: ["5:30-6:15pm (R)"],
+                Tuesday: ["5:30-6:15pm (R)"],
+                Wednesday: [],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["2:30-3:15pm (R)"],
+                Sunday: []
+            },
+            dryland: null
+        },
+        {
+            name: "Bronze",
+            schedule: {
+                Monday: ["6:15-7:00pm (R)"],
+                Tuesday: [],
+                Wednesday: ["6:15-7:00pm (R)"],
+                Thursday: [],
+                Friday: [],
+                Saturday: ["3:15-4:00pm (R)"],
+                Sunday: []
+            },
+            dryland: null
+        }
+    ]
+};
+
+window.BC_HIGHLIGHTED_GROUPS = ['Senior 1', 'Champs'];
+
